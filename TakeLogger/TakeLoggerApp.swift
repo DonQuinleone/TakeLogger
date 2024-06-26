b92b906e9f5a20afa3b0a0d670eb9dc5d1ef2fdd
Josh Quinlan <josh@quinlan.cloud>
2024-06-26 23:26:11 +0100

    Initial Commit

---
+++ TakeLogger/TakeLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeLoggerApp.swift
+//  TakeLogger
+//
+//  Created by Josh Quinlan on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TakeLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
